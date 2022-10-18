2df1ada58893d360134d2987dc3d08ef70f1c2f4
yelosolutions <96409558+yelosolutions@users.noreply.github.com>
2022-10-18 20:16:31 +0300

    models.user.py commit

--- models/user.py
+++ models/user.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-"""This is the user class"""
+""" holds class User"""
+
+from os import getenv
+
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, DateTime, Column, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
-    __tablename__ = 'users'
+    """Representation of a user"""
 
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship('Place', backref='user',
-                          cascade='all, delete-orphan')
-    reviews = relationship('Review', backref='user',
-                           cascade='all, delete-orphan')
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
