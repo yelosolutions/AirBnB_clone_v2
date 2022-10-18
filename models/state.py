26778d2e7b483cbe49cc50e0f6801f035f4255de
yelosolutions <96409558+yelosolutions@users.noreply.github.com>
2022-10-18 20:14:32 +0300

    models/state.py commit

--- models/state.py
+++ models/state.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""This is the state class"""
 from models.base_model import BaseModel, Base
-from models import storage_type
 from models.city import City
-from sqlalchemy import Column, String
+import models
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State class / table model"""
-    __tablename__ = 'states'
-    if storage_type == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state',
-                              cascade='all, delete, delete-orphan')
-    else:
-        name = ''
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete, delete-orphan",
+                          backref="state")
 
-        @property
-        def cities(self):
-            '''returns the list of City instances with state_id
-                equals the current State.id
-                FileStorage relationship between State and City
-            '''
-            from models import storage
-            related_cities = []
-            cities = storage.all(City)
-            for city in cities.values():
-                if city.state_id == self.id:
-                    related_cities.append(city)
-            return related_cities
+    @property
+    def cities(self):
+        objs = models.storage.all(City)
+        return [v for k, v in objs.items() if v.state_id == self.id]
